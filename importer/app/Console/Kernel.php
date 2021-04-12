19d038451884bc9f5690ae072045defc0de56b9a
Pär Thernström <par.thernstrom@gmail.com>
2021-04-12 20:21:48 +0200

    Lägg in task shedules för import av alla sidor

--- importer/app/Console/Kernel.php
+++ importer/app/Console/Kernel.php
@@ -24,7 +24,87 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+
+        /**
+         * Ofta, som nyheter + börs, sport.
+         * Starta försiktigt med var tredje minut.
+         */
+        $schedule->call(function () {
+            # nyheter, börs
+            #100-245
+            $this->importRange(100, 245);
+            # sport
+            #300-399
+            $this->importRange(300, 399);
+        })->everyThreeMinutes();
+
+        # ofta pga vad som är på tv just nu, typ varannan minut
+        #*/2 * * * * root cd /root/texttv-page-updater/ && php updater.php --pageRange 650-655 > 
+        $schedule->call(function () {
+            $this->importRange(650, 655);
+        })->everyFourMinutes();
+
+        ## nästan fresh, var femte minut eller så
+        # Lotto osv, hästar
+        #*/5 * * * * root cd /root/texttv-page-updater/ && php updater.php --pageRange 500-599 > 
+        $schedule->call(function () {
+            $this->importRange(500, 599);
+        })->everyTenMinutes();
+
+        # tv-tablå, Rimligt ofta
+        #6,12,19,31,42,49,57 * * * * root cd /root/texttv-page-updater/ && php updater.php --pageRange 600-649 > 
+        #*/19 * * * * root cd /root/texttv-page-updater/ && php updater.php --pageRange 656-669 > 
+        $schedule->call(function () {
+            $this->importRange(600, 649);
+            $this->importRange(656, 669);
+        })->everyFourMinutes();
+
+        # 670 - infosidor för tv
+        #3,18,28,39,47,58 * * * * root cd /root/texttv-page-updater/ && php updater.php --pageRange 670-699 > 
+        $schedule->call(function () {
+            $this->importRange(670, 699);
+        })->hourly();
+
+        # UR osv
+        #16,34,45,56 * * * * root cd /root/texttv-page-updater/ && php updater.php --pageRange 800-801 > 
+        $schedule->call(function () {
+            $this->importRange(800, 801);
+        })->everySixHours();
+
+        ## Halvofta, typ en gång i halvtimmen, väder..
+
+        # väder osv
+        #*/26 * * * * root sleep `numrandom /25..60/`s ; cd /root/texttv-page-updater/ && php updater.php --pageRange 400-439 > 
+        #12,26,39,42,59 * * * * root cd /root/texttv-page-updater/ && php updater.php --pageRange 400-439 > 
+        $schedule->call(function () {
+            $this->importRange(400, 439);
+        })->everyThirtyMinutes();
+
+        ## Sällan, en gång per dag-ish
+
+        # Dövinfo, slingan, teckenförklarings för börs, osv
+        #9 */2 * * * root cd /root/texttv-page-updater/ && php updater.php --pageRange 245-299 > 
+
+        # Gamla sport-rio-sidorna
+        #1 */2 * * * root cd /root/texttv-page-updater/ && php updater.php --pageRange 440-499 > 
+
+        # Inte så ofta/ändras sällan
+        #7 */2 * * * root cd /root/texttv-page-updater/ && php updater.php --pageRange 700-799 > 
+
+        $schedule->call(function () {
+            $this->importRange(245, 299);
+            $this->importRange(440, 499);
+            $this->importRange(700, 799);
+        })->daily();
+
+        # uppdateras aldrig?
+        #7 4 * * * root cd /root/texttv-page-updater/ && php updater.php --pageRange 900-999 > 
+        #7 */13 * * * root cd /root/texttv-page-updater/ && php updater.php --pageRange 802-899 > 
+        $schedule->call(function () {
+            $this->importRange(900, 999);
+            $this->importRange(802, 899);
+            $this->importRange(700, 799);
+        })->weekly();
     }
 
     /**

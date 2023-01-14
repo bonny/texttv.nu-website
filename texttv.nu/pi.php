67a363aae2f8d0ba8e2ce9f9f5547e8ec5afbbdc
Pär Thernström <par.thernstrom@gmail.com>
2023-01-14 19:35:58 +0100

    Visa endast phpinfo om rätt secret skickas med

--- texttv.nu/pi.php
+++ texttv.nu/pi.php
@@ -1,3 +1,15 @@
+<?php
+
+$VIEW_PHPINFO_SECRET = $_SERVER['VIEW_PHPINFO_SECRET'] ?? null;
+$VIEW_PHPINFO_QUERY_SECRET = $_GET['VIEW_PHPINFO'] ?? null;
+
+if ($VIEW_PHPINFO_SECRET !== $VIEW_PHPINFO_QUERY_SECRET) {
+    header("HTTP/1.1 401 Unauthorized");
+    echo "Näe.";
+    exit;
+}
+
+?>
 <p>before</p>
 <?php
 

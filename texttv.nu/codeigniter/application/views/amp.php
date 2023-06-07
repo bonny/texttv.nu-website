4d0009e5b070de8d2760f174e1de427af37741a5
Pär Thernström <par.thernstrom@gmail.com>
2023-06-07 15:12:42 +0200

    Sätt header location på Codeigniters vis

--- texttv.nu/codeigniter/application/views/amp.php
+++ texttv.nu/codeigniter/application/views/amp.php
@@ -49,8 +49,7 @@ if ($isMultiplePages || (isset($is_archive) && $is_archive)) {
 
 // 2023-06-07: Skickade vidare från /100/amp till /100 pga använder inte AMP aktivt längre.
 // $canonical = /100-102,211 osv.
-#header('Location: ' . $canonical, true, 301);
-#exit;
+$this->output->set_header('Location: ' . $canonical);
 
 ?><!doctype html>
 <html ⚡ lang="sv">

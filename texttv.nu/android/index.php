80ea25bbfcaa46e28da9b8e081c804793adc3b2b
Pär Thernström <par.thernstrom@gmail.com>
2023-08-01 09:05:02 +0200

    Lägg in GA4

--- texttv.nu/android/index.php
+++ texttv.nu/android/index.php
@@ -15,21 +15,14 @@
 			display: inline-block;
 		}
 	</style>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-J9BM4E3WHD"></script>
 	<script>
-		(function(i, s, o, g, r, a, m) {
-			i['GoogleAnalyticsObject'] = r;
-			i[r] = i[r] || function() {
-				(i[r].q = i[r].q || []).push(arguments)
-			}, i[r].l = 1 * new Date();
-			a = s.createElement(o),
-				m = s.getElementsByTagName(o)[0];
-			a.async = 1;
-			a.src = g;
-			m.parentNode.insertBefore(a, m)
-		})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-		ga('create', 'UA-181460-18', 'auto');
-		ga('require', 'linkid', 'linkid.js');
-		ga('send', 'pageview');
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
+
+	gtag('config', 'G-J9BM4E3WHD');
 	</script>
 </head>
 
